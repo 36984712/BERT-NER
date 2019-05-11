1df8684a0a6ee3fa8f240920aeb15e96eb08b32b
ruichenwen <u6130064@anu.edu.au>
2019-05-11 15:10:02 +1000

    renew sh

--- test.sh
+++ test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --get-user-env
-#SBATCH --job-name="dev 1e-4"
-#SBATCH --time=48:00:00
+#SBATCH --job-name="new"
+#SBATCH --time=12:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=4
-#SBATCH --gres=gpu:4
+#SBATCH --gres=gpu:1
 #SBATCH --mem=127GB
 
 module load python/3.7.1
